1917c7ad0ac2bc9f9b862431fe6a1f36ef46bda9
Nicholas Dixon <naseer2152@gmail.com>
2024-05-16 10:49:42 -0500

    Added lifecycle method to request and return values from the API.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,18 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state);
+      }
+      ));
+  }
+
   render() {
     return <div className="App">
       {
