83f11e84cd01b12ee25851161b55ced849a2bb5f
Nicholas Dixon <NickDixon32@proton.me>
2024-06-04 04:16:16 -0500

    Refactor: Implemented search functionality in componentDidMount and render.

--- src/App.js
+++ src/App.js
@@ -14,27 +14,46 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return {monsters: users}
-      },
-      () => {
-        console.log(this.state);
-      }
-      ));
+      .then((users) => 
+        this.setState(
+          () => {
+            return { monsters: users }
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
-    return <div className="App">
-      {
-        this.state.monsters.map((monster) => {
+    return (
+      <div className="App">
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filterMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filterMonsters };
+            })
+          }}
+        />
+        {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })
-      }
-    </div>
+        })}
+      </div>
+    );
   }
 }
 
