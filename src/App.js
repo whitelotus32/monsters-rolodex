61f656c7ab557fbe6941525ffd246991ee0fb704
Nicholas Dixon <NickDixon32@proton.me>
2024-06-27 21:44:39 -0500

    Dummy commit.

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
+        <h1 className="app-title">Monsters Rolodex</h1> 
 
 
         <searchBox 
